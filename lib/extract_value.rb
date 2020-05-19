c31295bb0c54f09238afb92bbae96d8d10c49f68
Joel AZEMAR <joel.azemar@gmail.com>
2020-05-19 11:37:47 +0200

    Initial Commit

---
+++ lib/extract_value.rb
@@ -0,0 +1,6 @@
+require "extract_value/version"
+
+module ExtractValue
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/extract_value/version.rb
@@ -0,0 +1,3 @@
+module ExtractValue
+  VERSION = "0.1.0"
+end
