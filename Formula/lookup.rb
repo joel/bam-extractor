da126df3dd9b4cc508a0e6926d01d6a30960047e
Joel Azemar <joel.azemar@gmail.com>
2020-12-01 18:26:29 +0100

    Release 0.4

--- Formula/lookup.rb
+++ Formula/lookup.rb
@@ -1,9 +1,9 @@
 class Lookup < Formula
   desc "Bank Account Movements Lookup"
   homepage "https://github.com/joel/homebrew-bam-lookup"
-  version "0.3"
+  version "0.4"
 
-  url "https://github.com/joel/homebrew-bam-lookup/archive/0.3.zip", :using => :curl
+  url "https://github.com/joel/homebrew-bam-lookup/archive/0.4.zip", :using => :curl
 
   def install
     bin.install "bin/lookup"
